1768cb063b326f0babdc93cc61a709c7b27e1b00
Rishab Raj <rr0948555@gmail.com>
2026-07-06 17:25:20 +0530

    Questions

--- Questions3.py
+++ Questions3.py
@@ -29,7 +29,89 @@
 
 # Print the table by taking the input
 
-n = int(input("Which Tab;e ypu want:-"))
+# n = int(input("Which Table you want:-"))
 
-for i in range(1,11):
-   print(f"{n} * {i} = {n*1}")
+# for i in range(1,11):
+#    print(f"{n} * {i} = {n*1}")
+
+
+
+
+# sum upto n terms
+
+# a = 10
+# a = a + 5  => a += 5
+# print(a)
+
+
+# n = int(input("Please tell your number:-"))
+
+# sum = 0
+
+# for i in range(1,n+1):
+#    sum = sum + i
+# print(f"Your sum is {sum}")
+
+
+
+
+
+
+# Factorial of a number
+
+# n = int(input('Enter the number:-'))
+
+# fact = 1
+
+# for i in range(1,n+1):
+#     fact = fact * i
+
+# print(f"Your factorial is {fact}")
+
+
+
+
+#  Print the sum of all even & odd numbers in a range separately.
+
+# n = int(input("Tell Your number:- "))
+# even = 0
+# odd = 0
+# for i in range(1,n+1):
+#     if i%2 == 0:
+#         even = even + i
+#     else:
+#         odd = odd + i
+
+# print(f"Your even and odd sum are {even} , {odd}")
+
+
+
+
+
+#  Print all the factors of a number
+
+
+# n = int(input("Which number factors you want :- "))
+
+# for i in range(1,n+1):
+#     if n%i == 0:
+#        print(i)
+
+
+
+
+
+
+#  Accept the number and check if it is a perfect number or not.
+
+
+n = int(input("Check your number is perfect or not :- "))
+sum = 0
+for i in range(1,n):
+    if n%i == 0:
+       sum = sum + i
+
+if sum == n:
+   print("Your number is perfect")
+else:
+   print("Your number is not a perfect number")
