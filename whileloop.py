557719988b83383edc1a5b3ec2f6620fab78c569
Rishab Raj <rr0948555@gmail.com>
2026-07-08 13:47:51 +0530

    while

---
+++ whileloop.py
@@ -0,0 +1,60 @@
+# a = 1
+
+# while a <= 30:
+#    print(a)
+#    a = a + 1
+
+
+
+#      Questions on while loop
+
+
+# Seperate each digit of a number and print it on the new line
+
+# a = 256
+# a = int(input("Tell your Number:-"))
+
+# while a > 0:
+#     print(a % 10)
+#     a = a // 10
+
+
+
+
+# Accept a number and print its reverse
+
+# a = int(input("Tell Your number:-"))
+
+# rev = 0
+
+# while a > 0:
+#     rev = rev * 10 + a % 10
+#     a = a // 10
+
+# print(rev)
+
+
+
+
+# Accept a number and check if it is a palindromic number (if number and its reverse are equal)
+
+a = int(input("Tell your Number:-"))
+
+copy = a
+rev = 0
+
+while a > 0:
+    rev = rev * 10 + a % 10
+    a = a // 10
+    
+if copy == rev:
+    print("Palindromic Number")
+else:
+    print("Not a Palindromic  number")
+
+
+
+
+
+
+# Create a random number guessing game with python
