665a6a474907d840b456b3f11caed2abd1b1aab4
Rishab Raj <rr0948555@gmail.com>
2026-07-11 16:40:57 +0530

    Functions

--- Functions.py
+++ Functions.py
@@ -32,15 +32,32 @@
 
 # Default arguments
 
-def sum(a,b=34):
-    print(f"The Sum is {a+b}")
+# def sum(a,b=34):
+#     print(f"The Sum is {a+b}")
   
-sum(12)
+# sum(12)
 
 
 #  Keyword arguments
 
-def hello(name,age):
-    print(f"Your name is {name} and your age is {age}")
+# def hello(name,age):
+#     print(f"Your name is {name} and your age is {age}")
 
-hello(age = 22, name = "Alex")
+# hello(age = 22, name = "Alex")
+
+
+
+# Palindrome function
+
+def palindrome(st):
+    rev = ""
+    for i in range(len(st)-1,-1,-1):
+        rev = rev + st[i]
+        
+    if rev == st:
+        print("{st} is a Palindrome")
+    else:
+        print("{st} is not a Palindrome")
+        
+palindrome("NAMAN")
+palindrome("CURSOR")
