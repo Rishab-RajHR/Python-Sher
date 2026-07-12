b6a4556b568f6cdadbab2327f042917a044b8560
Rishab Raj <rr0948555@gmail.com>
2026-07-12 13:56:54 +0530

    List Questions

---
+++ DataStructure/Questions.py
@@ -0,0 +1,57 @@
+# Print positive and negative elements of an list
+
+
+g = [-45,67,12,-68,-69,34]
+
+print("Positive elements are")
+for c in g:
+    if c >= 0:
+        print(c)
+        
+print("Negative elements are")
+
+for c in g:
+    if c < 0:
+        print(c)
+
+
+
+
+# Mean of list elements
+
+l = [12,435,67,89,23,25,69]
+
+sum = 0
+
+for i in l:
+    sum = sum + i
+    
+print(sum/len(l))
+
+
+
+
+# Find the greatest element and print its index too
+
+t = [4, 67, 128, 34, 88, 6, 20]
+
+largest = t[0]
+index = 0
+
+for i in range(len(t)):
+    if t[i] > largest:
+        largest = t[i]
+        index = i
+        
+print(f"Your largest number is {largest} at index {index}")
+
+
+
+
+
+
+# Find the second greatest element
+
+
+
+# Check if List is sorted or not
--- DataStructure/main.py
+++ DataStructure/main.py
@@ -24,3 +24,54 @@ for i in range(len(a)):
 
 for i in a:
    print(i)    
+   
+   
+# List of methods
+
+# print(dir(list))
+
+
+# Append => Add the value at the last
+
+l = [1,2,3,4,5]
+
+l.append(6)
+l.append(7)
+
+print(l)
+
+
+
+# insert the value at the position
+
+k = [1,3,4,5]
+
+k.insert(1,2)
+# l.remove(3)  => removes the first occurence
+
+print(k)
+
+
+# Accessing the index value and updating the value
+m = [1,2,3,2,4,5]
+
+m[0] = 10
+print(m)
+
+
+
+
+# Using the for loop to access the value
+
+g = [-45,67,12,-68,-69,34]
+
+print("Positive elements are")
+for c in g:
+    if c >= 0:
+        print(c)
+        
+print("Negative elements are")
+
+for c in g:
+    if c < 0:
+        print(c)
