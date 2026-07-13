636b987dd089f5207708b0739bc236dab990265d
Rishab Raj <rr0948555@gmail.com>
2026-07-13 14:32:09 +0530

    Set

---
+++ DataStructure/set.py
@@ -0,0 +1,5 @@
+# A set in Python is an unordered, mutable collection of unique elements. It automatically removes duplicate values and is commonly used for membership testing and mathematical set operations.
+
+s = {1,2,3,4,4,5}
+
+print(s)
