b98dbe85989f4a27bf4e6492bed8a707a56149a1
Rishab Raj <rr0948555@gmail.com>
2026-07-13 14:27:34 +0530

    Tuple

---
+++ DataStructure/Tuple.py
@@ -0,0 +1,35 @@
+# A tuple in Python is an ordered, immutable collection of items. Once a tuple is created, its elements cannot be changed, added, or removed.
+
+
+# a = (1,2,3,4,5)
+
+# print(type(a))
+
+
+
+
+a = (1,2,3,4,5,5,5.5,print(),"hello")
+
+
+# Traversal in tuple
+# for i in range(len(a)):
+#     print(i)
+    
+    
+# Index occurences
+# index = a.index(5)
+# print(index)
+
+
+# Count occurences
+# count = a.count(5)
+# print(count)
+
+
+
+# a = (1)   # Integer
+# a = (1,)  # Tuple
+
+# Tuple unpacking
+a,b,c,d = (1,2,3,4)
+print(b)
