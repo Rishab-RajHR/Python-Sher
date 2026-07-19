985198c7da3ed8e7a3ecd17289cc4e7ccd01759f
Rishab Raj <rr0948555@gmail.com>
2026-07-19 17:04:49 +0530

    Dictionary

---
+++ DataStructure/dict.py
@@ -0,0 +1,14 @@
+d = {1:'Hello',2:34}
+print(type(d))
+
+# Keys must be unique and values are not unique
+
+# Keys and values can be of any type
+
+
+d = {10:100, 20:200, 30:300, 40:400}
+
+# d[10] = 100
+# print(d)
+
+print(d[10])
