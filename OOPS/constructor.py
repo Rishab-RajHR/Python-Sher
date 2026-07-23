5d675d020f6265446f61eb9247c262cc4df37ca8
Rishab Raj <rr0948555@gmail.com>
2026-07-23 14:28:44 +0530

    Contructor

--- OOPS/constructor.py
+++ OOPS/constructor.py
@@ -1,8 +1,12 @@
 class Factory:
     def __init__(self,material,zips,pockets):
+        print(self)
         self.material = material
         self.zips = zips
         self.pockets = pockets
+        
+    def show(self):
+        print(f"Your object details are {self.material}, {self.pockets}, {self.zips}")
         
 reebok = Factory("Leather",3,2)
 
@@ -10,3 +14,5 @@ campus = Factory("Nylon",3,3)
 
 print(reebok.pockets)
 print(campus.pockets)
+
+reebok.show()
