3ec3022831f5acd4a988d64c1d7b8f32b5f11ad8
Rishab Raj <rr0948555@gmail.com>
2026-07-24 11:43:15 +0530

    Attribute

---
+++ OOPS/attribute.py
@@ -0,0 +1,24 @@
+# Attribute are function created within the class
+
+class Animal:
+   name = "Lion"  # class attribute
+   
+   def __init__(self, age):
+      self.age = age  # Instance attribute
+      
+   def show(self):  # instance method
+     print(f"How are you old {self.age}")
+     
+    
+   @classmethod
+   def hello(cls):
+        print("How are you Brother")
+        
+   @staticmethod
+   def static():
+       print("How are you")
+       
+       
+obj = Animal(12)
+
+obj.show()
