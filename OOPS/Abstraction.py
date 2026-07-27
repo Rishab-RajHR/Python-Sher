843ddaa5a3a8c2ae20a9f20ae150c37e73633dc6
Rishab Raj <rr0948555@gmail.com>
2026-07-27 14:21:58 +0530

    Abstraction

---
+++ OOPS/Abstraction.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+class abstract(ABC):
+    @abstractmethod
+    def perimeter(self):
+        pass
+      
+    @abstractmethod
+    def area(self):
+        pass
+      
+class Square(abstract):
+    def __init__(self,side):
+        self.side = side
+        
+class Circle(abstract):
+    def __init__(self,radius):
+        self.radius = radius
+        
+    def perimeter(self):
+        print("I have created")
+      
+    def area(self):
+        print("I have created this")
+        
+obj = Circle(3)
--- OOPS/Private.py
+++ OOPS/Private.py
@@ -1,14 +1,28 @@
 #  Private Method that cannot be accessed from anywhere
 
-class Factory:
-   __a = "Pune"
+# class Factory:
+#    __a = "Pune"
    
+#    def show(self):
+#       print("Hello I am a Pune Factory")
+      
+# class Bhopal(Factory):
+#    def show2(self):
+#       print(super().__a)
+      
+# obj = Bhopal()
+# obj.show2()
+
+
+
+class Demo:
+   def __init__(self):
+      self.name = "Public Member"   # Public
+      self._age = 21                # Protected
+      self.__salary = 50000         # Private
+      
    def show(self):
-      print("Hello I am a Pune Factory")
-      
-class Bhopal(Factory):
-   def show2(self):
-      print(super().__a)
-      
-obj = Bhopal()
-obj.show2()
+       print("Inside the class:")
+       print("Public:", self.name)
+       print("Protected:", self._age)
+       print("Private:", self.__salary)
