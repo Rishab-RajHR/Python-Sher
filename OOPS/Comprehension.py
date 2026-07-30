543287f8c48f593ccc34baa3ac5b9cae05205dcf
Rishab Raj <rr0948555@gmail.com>
2026-07-30 11:58:35 +0530

    Lambda

--- OOPS/Comprehension.py
+++ OOPS/Comprehension.py
@@ -1,8 +1,23 @@
 # List Comprehension --> Gets the result in one line
 
-l = []
-for i  in range(1,21):
-    if i % 2==0:
-        l.append(i)
+# l = []
+# for i  in range(1,21):
+#     if i % 2==0:
+#         l.append(i)
         
+# print(l)
+
+
+# Write all the things in one line
+
+# l = [i for i in range(1,21) if i % 2==0]
+
+# print(l)
+
+
+
+#  Making the Dictionary Comprehension
+
+l = { i : i**2 for i in range(1,10)}
+
 print(l)
---
+++ OOPS/Lambda.py
@@ -0,0 +1,18 @@
+# def addition(a, b):
+#     print(a + b)
+    
+# addition(12, 13)
+
+
+
+
+# Lambda Function in single line
+
+# addition = lambda a,b : a + b
+
+# print(addition(12,13))
+
+
+addition = lambda a : "even" if a%2 == 0 else "odd"
+
+print(addition(12))
